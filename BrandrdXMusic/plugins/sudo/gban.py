495df56cf881488b2fd09b33ea1bc77c5ec96ca0
ARJUN <pachangedinanath@gmail.com>
2026-03-09 01:59:21 +0530

    Update gban.py

--- BrandrdXMusic/plugins/sudo/gban.py
+++ BrandrdXMusic/plugins/sudo/gban.py
@@ -103,7 +103,7 @@ async def global_ban(client, message: Message, _):
 # ─────────────────────────────
 @app.on_message(filters.command(["upliftstrike"], prefixes=["/", "!", "."]) & SUDOERS)
 @language
-async def global_un(client, message: Mesungban_):
+async def global_un(client, message: Message_):
     if not message.reply_to_message and len(message.command) != 2:
         return await message.reply_text(_["general_1"])
 
