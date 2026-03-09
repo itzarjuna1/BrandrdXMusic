091b5d47bb3e8999ae4b3cac295ebb516c41daa0
ARJUN <pachangedinanath@gmail.com>
2026-03-09 09:32:29 +0530

    Create chats_db.py

---
+++ BrandrdXMusic/mongo/chats_db.py
@@ -0,0 +1,14 @@
+from BrandrdXMusic import mongodb
+
+chatsdb = mongodb["network_chats"]
+
+async def add_chat(chat_id: int):
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        await chatsdb.insert_one({"chat_id": chat_id})
+
+async def get_chats():
+    chats = []
+    async for chat in chatsdb.find():
+        chats.append(chat["chat_id"])
+    return chats
