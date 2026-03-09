528e8d8cf44dcace0c2835e1b79309171aa6bd23
ARJUN <pachangedinanath@gmail.com>
2026-03-09 12:05:00 +0530

    Update __init__.py

--- BrandrdXMusic/__init__.py
+++ BrandrdXMusic/__init__.py
@@ -27,4 +27,4 @@ Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
 
-APP = "Roshni_X_Music_Bot"  # connect music api key "Dont change it"
+APP = "BRANDED_KUDI_BOT"  # connect music api key "Dont change it"
